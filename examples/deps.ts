8443e421591b6d1cc2f355550f346375c9a78a71
herudi <herudi7@gmail.com>
2021-07-06 09:53:55 +0700

    add

--- examples/deps.ts
+++ examples/deps.ts
@@ -8,6 +8,8 @@ export {
 } from "https://deno.land/x/dero@1.2.1/mod.ts";
 export type { Handler } from "https://deno.land/x/dero@1.2.1/mod.ts";
 export { UnauthorizedError } from "https://deno.land/x/dero@1.2.1/error.ts";
+window.DeroMetadata = window.DeroMetadata;
+console.log("META => ", window.DeroMetadata)
 export {
   ApiBearerAuth,
   ApiDocument,
