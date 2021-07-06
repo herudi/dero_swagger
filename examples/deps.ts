acb793d6db206b90b978b6ef1323cfcd1a5a8113
herudi <herudi7@gmail.com>
2021-07-06 08:31:50 +0700

    examples

--- examples/deps.ts
+++ examples/deps.ts
@@ -8,7 +8,6 @@ export {
 } from "https://deno.land/x/dero@1.2.1/mod.ts";
 export type { Handler } from "https://deno.land/x/dero@1.2.1/mod.ts";
 export { UnauthorizedError } from "https://deno.land/x/dero@1.2.1/error.ts";
-window.DeroMetadata = window.DeroMetadata;
 export {
   ApiBearerAuth,
   ApiDocument,
